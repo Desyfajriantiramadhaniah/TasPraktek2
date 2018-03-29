06a64bfd021b05e9ff4e30e83fc57f277d0f0a74
desyfajrianti <dessy.kps@gmail.com>
2018-03-29 16:20:49 +0800

    membuat atribut dan behavior

--- src/taspraktek2/Tas2.java
+++ src/taspraktek2/Tas2.java
@@ -1,4 +1,12 @@
 package taspraktek2;
 public class Tas2 {
+    double hargatas;
+    double jumlahtas;
     
+    void cetakinfo(){
+        System.out.println("*************************");
+        System.out.println("hargatas : "+hargatas);
+        System.out.println("jumlahtas : "+jumlahtas);
+        System.out.println("**************************");
+    }
 }
