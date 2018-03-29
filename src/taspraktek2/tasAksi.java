ca2c60651d17dd3dd46d42b27b4d2153f6313ad7
desyfajrianti <dessy.kps@gmail.com>
2018-03-29 16:50:51 +0800

    method tanpa nilai balik

--- src/taspraktek2/tasAksi.java
+++ src/taspraktek2/tasAksi.java
@@ -5,7 +5,7 @@ public class tasAksi {
         Tas2 w1 = new Tas2();
        w1.hargatas = 600000;
        w1.jumlahtas = 5;
-       
+      
        w1.cetakinfo();
       System.out.println("Hasil keuntungan ="+w1.hitungkeuntungan());
       w1.cetakkeuntungan();
